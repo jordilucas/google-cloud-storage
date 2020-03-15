67350f3ec22f8e1092a82fe45a6ee2feede0b619
Jordi <jordilucas3@gmail.com>
2020-03-15 10:13:22 -0300

    adicionado url statica

--- googlecloud/urls.py
+++ googlecloud/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from googlecloud import settings
 from image.api.viewsets import AtracoesViewSet
 
 router = routers.DefaultRouter()
@@ -24,4 +26,4 @@ router.register(r'image', AtracoesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
